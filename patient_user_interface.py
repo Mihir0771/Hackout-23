cb588f791aa97257b436fb342e4dad9fd141d1c8
KUNAL PUNDHIR <141947166+augustbeyondinfinity@users.noreply.github.com>
2023-08-27 03:43:14 +0530

    patient_user_interface.py

--- patient_user_interface.py
+++ patient_user_interface.py
@@ -2,9 +2,10 @@ import sqlite3
 from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
-import Appointment
 
 global password, username, age, date_of_birth, aadhar_card_number, last_name, first_name
+
+
 # [('U', 'S', '12/08/2022', 1, 102938475638, '123')]
 
 
@@ -36,59 +37,58 @@ def sign_up():
 
         conn.close()
 
+        messagebox.showwarning("oyee", "hiiii")
+        root.destroy()
+
     root = Toplevel()
     root.attributes("-fullscreen", True)
-
-    comp_image = ImageTk.PhotoImage(Image.open("comp.jpg"))
-
-    comp_label = Label(root, image=comp_image)
-    comp_label.pack()
+    root.config(bg="light green")
 
     # First Name
-    first_name_label = Label(root, text="First Name")
-    first_name_label.place(relx=0.15, rely=0.2)
+    first_name_label = Label(root, text="First Name",font="Cambria 24 bold",bg="light green")
+    first_name_label.place(relx=0.14, rely=0.2)
 
     first_name_entry_box = Entry(root)
-    first_name_entry_box.place(relx=0.25, rely=0.2)
+    first_name_entry_box.place(relx=0.27, rely=0.22)
 
     # Last Name
-    last_name_label = Label(root, text="Last Name")
+    last_name_label = Label(root, text="Last Name",font="Cambria 24 bold",bg="light green")
     last_name_label.place(relx=0.65, rely=0.2)
 
     last_name_entry_box = Entry(root)
-    last_name_entry_box.place(relx=0.75, rely=0.2)
+    last_name_entry_box.place(relx=0.79, rely=0.22)
 
     # Date of Birth
-    date_of_birth_label = Label(root, text="Date of Birth")
-    date_of_birth_label.place(relx=0.15, rely=0.4)
+    date_of_birth_label = Label(root, text="Date of Birth",font="Cambria 24 bold",bg="light green")
+    date_of_birth_label.place(relx=0.12, rely=0.4)
 
     date_of_birth_entry_box = Entry(root)
-    date_of_birth_entry_box.place(relx=0.25, rely=0.4)
+    date_of_birth_entry_box.place(relx=0.3, rely=0.41)
 
     # Age
-    age_label = Label(root, text="Age")
+    age_label = Label(root, text="Age",font="Cambria 24 bold",bg="light green")
     age_label.place(relx=0.65, rely=0.4)
 
     age_entry_box = Entry(root)
-    age_entry_box.place(relx=0.75, rely=0.4)
+    age_entry_box.place(relx=0.72, rely=0.42)
 
     # Aadhar Card Number
-    aadhar_card_number_label = Label(root, text="Aadhar-Card Number")
+    aadhar_card_number_label = Label(root, text="Aadhar-Card Number",font="Cambria 24 bold",bg="light green")
     aadhar_card_number_label.place(relx=0.15, rely=0.6)
 
     aadhar_card_number_entry_box = Entry(root)
-    aadhar_card_number_entry_box.place(relx=0.25, rely=0.6)
+    aadhar_card_number_entry_box.place(relx=0.19, rely=0.66)
 
     # Password
-    password_label = Label(root, text="Password")
+    password_label = Label(root, text="Password",font="Cambria 24 bold",bg="light green")
     password_label.place(relx=0.65, rely=0.6)
 
     password_entry_box = Entry(root, show='*')
-    password_entry_box.place(relx=0.75, rely=0.6)
+    password_entry_box.place(relx=0.69, rely=0.66)
 
     # Submit Button
-    submit_button = Button(root, text="Submit", command=lambda: [submit(), root.destroy()])
-    submit_button.place(relx=0.5, rely=0.8)
+    submit_button = Button(root, text="Submit",font="Cambria 17 bold", command=lambda: [submit(), root.destroy()])
+    submit_button.place(relx=0.46, rely=0.8)
 
     root.mainloop()
 
@@ -104,7 +104,7 @@ def dashboard():
     profile_first_name_label.place(relx=0.4, rely=0.2)
 
     profile_last_name_label = Label(win, text=f"Last name: {last_name}")
-    profile_last_name_label.place(relx=0.6, rely=0.2)
+    profile_last_name_label.place(relx=0.4, rely=0.2)
 
     profile_date_of_birth_label = Label(win, text=f"Date of Birth: {date_of_birth}")
     profile_date_of_birth_label.place(relx=0.4, rely=0.4)
@@ -118,14 +118,8 @@ def dashboard():
     profile_password_label = Label(win, text=f"Password: {password}")
     profile_password_label.place(relx=0.6, rely=0.6)
 
-    def appointment():
-        app = Label(win, text=Appointment.appointment_confirmation(aadhar_card_number))
-        app.place(relx=0.6, rely=0.8)
-        win.mainloop()
-
-    book_appointment_button = Button(win, text='Book Appointment', command=lambda: [Appointment.appointment_confirmation(aadhar_card_number),
-                                                      appointment()])
-    book_appointment_button.place(relx=0.4, rely=0.8)
+    book_appointment_button = Button(win, text='Book Appointment')
+    book_appointment_button.place(relx=0.4, rely=0.8)  ## KRISH
 
     history_button = Button(win, text="Medical History")
     history_button.place(relx=0.6, rely=0.8)  ## KRISH
@@ -148,40 +142,38 @@ def login():
     win = Tk()
     win.attributes("-fullscreen", True)
 
-    id_label = Label(win, text="ID")
+    id_label = Label(win, text="ID",font="Cambria 14 bold")
     id_label.place(relx=0.4, rely=0.4)
 
-    id_entry_box = Entry(win, show="*")
+    id_entry_box = Entry(win)
     id_entry_box.place(relx=0.5, rely=0.4)
 
-    password_label = Label(win, text="Password")
+    password_label = Label(win, text="Password",font="Cambria 17 bold")
     password_label.place(relx=0.4, rely=0.6)
 
     password_entry_box = Entry(win)
     password_entry_box.place(relx=0.5, rely=0.6)
 
-    enter_button = Button(win, text="Enter", command=enter)
+    enter_button = Button(win, text="Enter", command=enter,font="Cambria 15 bold")
     enter_button.place(relx=0.5, rely=0.8)
 
     win.mainloop()
 
 
 window = Tk()
-window.config(bg="#C4DEF6")
+img = PhotoImage(file="doctor.png")
+label = Label(window,image=img)
+label.place(x=0, y=0)
 window.attributes("-fullscreen", True)
 
 login_button = Button(window, text="Login", font=("Agency FB", 26), command=lambda: [login()])
-login_button.place(relx=0.4, rely=0.5)
+login_button.place(relx=0.7, rely=0.33)
 
 sign_up_button = Button(window, text="Sign Up", font=("Agency FB", 26), command=lambda: [sign_up()])
-sign_up_button.place(rely=0.5, relx=0.5)
+sign_up_button.place(rely=0.49, relx=0.7)
 
 quit_button = Button(window, text="Quit", font=("Agency FB", 26), command=quit)
-quit_button.place(relx=0.5, rely=0.7)
+quit_button.place(relx=0.7, rely=0.69)
 
-doctor_image = ImageTk.PhotoImage(Image.open("doctor.jpg"))
-
-image_label = Label(window, image=doctor_image, borderwidth=0)
-image_label.place(relx=0.1, rely=0.1)
 
 window.mainloop()
